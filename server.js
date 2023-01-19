ace4ae07b107bbaf4d3720e256d812cafbbe7624
jane-d-alt <51848939+jane-d-alt@users.noreply.github.com>
2023-01-19 20:12:29 +0000

    Update server.js

--- server.js
+++ server.js
@@ -8,16 +8,28 @@ const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
 
+let bcrypt = require('bcrypt');
+
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
 
-
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+  /*Store hash in your db*/
+  //console.log(hash)
+  bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+    /*res == true or false*/
+    //console.log(res)
+  });
+});
 
 //END_ASYNC
 
 //START_SYNC
 
-
+var hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+console.log(hash)
+var result = bcrypt.compareSync(myPlaintextPassword, hash);
+console.log(result)
 
 //END_SYNC
 
